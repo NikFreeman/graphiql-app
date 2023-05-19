703ec873e17c501740a3a5929ced40bb547e87c0
Nikolay Hancharonok <classic76@tut.by>
2023-05-19 11:33:47 +0300

    feat: ProtectedRoute render Loading

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,6 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 
 export function useAuth() {
   const [user, loading, error] = useAuthState(auth);
+  console.log('user->', user, 'loading->', loading);
   const toast = useToast();
   if (error) {
     toast({
