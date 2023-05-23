6d6f154cd7cc0227d412175d3d11f2f00cc3ebfa
Nikolay Hancharonok <classic76@tut.by>
2023-05-23 12:18:08 +0300

    fix: deploy

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -10,7 +10,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-console.log('firebase->', firebaseConfig);
 
 const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
