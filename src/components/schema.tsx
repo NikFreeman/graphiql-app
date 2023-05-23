22e80d03357e57426d4430d73b7a643ed4e65f56
Bonus156 <Bonus.by@mail.ru>
2023-05-23 22:33:19 +0300

    feat: add styles for schema, add key atr for arg in GetTypes, add translate for query & details

--- src/components/schema.tsx
+++ src/components/schema.tsx
@@ -11,6 +11,7 @@ import {
 
 import { getSchema } from '../helpers/variables';
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 type Arg = {
   name: string;
@@ -85,7 +86,7 @@ function GetTypes({ field }: DrawTreeProps) {
             {field.args.length ? '(' : ''}
             {field.args.map((arg) => {
               return (
-                <>
+                <Text as="span" key={arg.name}>
                   <Text as="span" color={ARG_NAME_COLOR}>
                     {' '}
                     {arg.name}:{' '}
@@ -93,7 +94,7 @@ function GetTypes({ field }: DrawTreeProps) {
                   <Text as="span" color={ARG_TYPE_NAME_COLOR}>
                     {getArgTypeName(arg)}
                   </Text>
-                </>
+                </Text>
               );
             })}
             {field.args.length ? '): ' : ': '}
@@ -124,6 +125,7 @@ export default function Schema() {
     }
     loadDoc();
   }, []);
+  const { t } = useTranslation();
   return (
     <>
       {isSchemaLoaded && (
@@ -132,17 +134,17 @@ export default function Schema() {
             <h2>
               <AccordionButton>
                 <Box as="span" flex="1" textAlign="left" fontSize={'18px'}>
-                  Query: {schema.types[0].name}
+                  {t('query')}: {schema.types[0].name}
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
-              <Box as="span" flex="1" textAlign="left" fontSize="sm">
+              <Box as="span" flex="1" textAlign="justify" fontSize="sm">
                 {schema.types[0].description}
               </Box>
             </h2>
             <AccordionPanel pb={4} px={0}>
               <Text as="h3" mb={'5px'} fontSize={'18px'} color={H3_COLOR}>
-                Queries
+                {t('queries')}
               </Text>
               <SchemaTree typeName={schema.types[0].name} />
             </AccordionPanel>
@@ -162,17 +164,34 @@ interface DrawTreeProps {
 function SchemaTree({ field, typeName }: DrawTreeProps) {
   const fieldTypeName = typeName ? typeName : field && getTypeName(field);
   const type = schema.types.find((type) => type.name === fieldTypeName);
+  const { t } = useTranslation();
   if (type && type.fields) {
     return (
       <Accordion allowToggle>
         {type.fields.map((field) => {
           return (
-            <AccordionItem key={field.name}>
+            <AccordionItem
+              key={field.name}
+              border={'solid 1px'}
+              borderColor={'gray.200'}
+              py={'5px'}
+              my={'5px'}
+              pl={'2px'}
+              ml={'1px'}
+              pr={'0'}
+              mr={'0'}
+              borderRight={'0px'}
+              borderBottomRightRadius="0.5rem"
+              borderBottomLeftRadius="0"
+              borderTopLeftRadius="1rem"
+              borderTopRightRadius="0"
+              textAlign="justify"
+            >
               {({ isExpanded }) => (
                 <>
-                  <h2>
+                  <Text as="h2">
                     <AccordionButton>
-                      <Box as="span" flex="1" textAlign="left">
+                      <Box as="span" flex="1" textAlign="justify">
                         {field.name}
                       </Box>
                       <AccordionIcon />
@@ -180,25 +199,25 @@ function SchemaTree({ field, typeName }: DrawTreeProps) {
                     <Text
                       as="span"
                       flex="1"
-                      textAlign="left"
+                      textAlign="justify"
                       fontSize="sm"
                       color={DESCRIPTION_COLOR}
                     >
                       {field.description}
                     </Text>
-                  </h2>
+                  </Text>
                   <AccordionPanel pb={4} px={0}>
                     <GetTypes field={field} />
                     <Box mt={8}>
                       <Text as="h3" mb={'5px'} fontSize={'18px'} color={H3_COLOR}>
-                        Type details
+                        {t('details')}
                       </Text>
                       {isExpanded && <SchemaTree field={field} />}
                     </Box>
                     {!!field.args.length && (
                       <Box mt={8}>
                         <Text as="h3" mb={'5px'} fontSize={'18px'} color={H3_COLOR}>
-                          Arguments
+                          {t('arguments')}
                         </Text>
                         <Accordion allowToggle>
                           {field.args.map((arg: Arg) => {
@@ -212,9 +231,9 @@ function SchemaTree({ field, typeName }: DrawTreeProps) {
                               <AccordionItem key={arg.name}>
                                 {({ isExpanded }) => (
                                   <>
-                                    <h2>
+                                    <Text as="h2">
                                       <AccordionButton>
-                                        <Box as="span" flex="1" textAlign="left">
+                                        <Box as="span" flex="1" textAlign="justify">
                                           <Text as="span" color={TYPE_NAME_COLOR}>
                                             {' '}
                                             {arg.name}:{' '}
@@ -225,8 +244,8 @@ function SchemaTree({ field, typeName }: DrawTreeProps) {
                                         </Box>
                                         <AccordionIcon />
                                       </AccordionButton>
-                                    </h2>
-                                    <AccordionPanel pb={4}>
+                                    </Text>
+                                    <AccordionPanel pb={4} px={0}>
                                       {isExpanded && typeForArg?.kind === 'SCALAR' && (
                                         <Text
                                           as="span"
@@ -245,7 +264,7 @@ function SchemaTree({ field, typeName }: DrawTreeProps) {
                                             fontSize={'18px'}
                                             color={H3_COLOR}
                                           >
-                                            Type details
+                                            {t('details')}
                                           </Text>
                                           {typeForArg.inputFields?.map((inputField) => {
                                             return (
@@ -253,7 +272,7 @@ function SchemaTree({ field, typeName }: DrawTreeProps) {
                                                 {({ isExpanded }) => (
                                                   <>
                                                     <AccordionButton>
-                                                      <Box as="span" flex="1" textAlign="left">
+                                                      <Box as="span" flex="1" textAlign="justify">
                                                         {inputField.name +
                                                           ': ' +
                                                           getArgTypeName(inputField)}
